a758e3dc8fa5667f9772595f101b92c62cf81efd
nata2283 <nata_22_14@mail.ru>
2016-05-30 22:04:00 +0300

    added corection js15-16

--- js15-16/js/script.js
+++ js15-16/js/script.js
@@ -1,70 +1,38 @@
    jQuery(document).ready(function(){
         
-        jQuery('#my-google').submit(function(){
+        jQuery('#my-google').submit(function(e){
         
-            var urlFull = 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&key=ABQIAAAACKQaiZJrS0bhr9YARgDqUxQBCBLUIYB7IF2WaNrkYqF0tBovNBQFDtM_KNtb3xQxWff2mI5hipc3lg&rsz=large&q='+ encodeURIComponent(jQuery('#area-search').val()) + '&callback=GoogleCallback&context=?';
-            
-            jQuery.ajax({
-                // AJAX-specified URL
-               url: urlFull,
-               dataType : "jsonp",
-                  
-               // Handle the success event
-               success: function (data, textStatus) {
-    				var ul = document.createElement("ul");
-                    // build results set
-                    jQuery.each(data.results,
-        					function(i, val) {
-                                var $element = $('img');
-                                var $myForm = $('form');
-                                var $inputArea = $('#area-search');
-                                var $button = $('#search');
-                       // $element.remove();
-                              $element.css({
-                                    display: 'inline-block',
-                                    width: '70px',
-                                    height: '30px',
-                                    margin: '0 20px',
-                                    float: 'left',
-                              });
-                              $myForm.css({
-                                    
-                                    width: '100%',
-                                    height: '60px',
-                                    margin: '0px 0 0 0',
-                                    background: '#cecece',
-                                    marginBottom: '30px',
-                                    paddingTop: '20px',
-                                  
-                              });
-                              $inputArea.css({
-                                    width: '560px',
-                                    height: '30px',
-                                    margin: '0px 0 0 0',
-                                    background: '#fff',
-                                    float: 'left',
-                                    marginRight: '10px',
-                              });    
-                              $button.css({
-                                   
-                                    margin: '0px',
-                                   
-                              });
-        						var li = document.createElement("li");
-        						
-        						li.innerHTML = '<a href="'+val.url+'" title="'+val.url+'" target="_blank">'+val.title+"</a>  "+val.content;        						
-        						
-        						ul.appendChild(li);
-        					}
-        				);
-    				jQuery('#results').html(ul);
-    		    }				
-    		});
-            return false;
-        });
-        
-    });
+ var search = $('#area-search').val();
+  
+    e.preventDefault();
+    if(search){
+        $('#area-search').val('');
+     $.ajax({
+        type: 'GET',
+        url:'http://api.pixplorer.co.uk/image?word=' + search + '&amount=7&size=L',
+
+        dataType: 'json',
+        success: function(data){
+
+        var arrImageurl = _.map(data.images, 'imageurl');
     
+        $('.grid-item').each(function(i){
+     
+        $(this).css('backgroundImage', 'url('+arrImageurl[i]+')');
+       
+   });
+   
+   $('.grid-item > div > p').text(search);
+
+        },
+        error: function(){
+            console.log('Request is bad');
+        }          
+    });
+  }
+    
+    });
+ });
     function GoogleCallback (func, data) {
         window[func](data);
     }
